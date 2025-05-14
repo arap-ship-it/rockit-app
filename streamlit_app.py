17e41de144a2b926c05c06bc11b859d8c7992247
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-14 15:10:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ st.title("~BARUDAWGH~")
 st.write(
     "BEJA KA BARUDAWGH ONE KOMANDOᯓ ✈︎"
 )
-st.image("IMG_20250227_175635_386.jpg", width=200)
+st.image("aleta02150.jpg", width=200)
 st.balloons() 
