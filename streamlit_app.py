8b52c2af51f40c74824d41f8214a608ad46b794b
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-14 15:02:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("~WARPOL~")
 st.write(
-    "BEJA KA BARUDAWGH ONE KOMANDO"
+    "BEJA KA BARUDAWGH ONE KOMANDOᯓ ✈︎"
 )
 st.image("IMG_20250227_175635_386.jpg", width=200)
 st.balloons() 
