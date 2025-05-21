09dce451ca81503bd6625cdade89c4f8d5eeb93d
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-21 15:22:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+st.title("Duran Duran")
 st.page_link("streamlit_app.py", label="Home", icon="🏠")
 st.page_link("page1.py", label="Page1", icon="1️⃣")
 st.page_link("page2.py", label="Page2", icon="2️⃣", disabled=True)
