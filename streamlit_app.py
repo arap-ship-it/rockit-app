d77e7f27c241be7d917fbe66c7c60a4c87351f75
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-21 15:03:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,6 @@ if selected is not None:
     import streamlit as st
 
 st.page_link("streamlit_app.py", label="Home", icon="🏠")
-st.page_link("page1. py", label="Page 1", icon="1️⃣")
-st.page_link("page2. py", label="Page 2", icon="2️⃣", disabled=True)
+st.page_link("page1.py", label="Page 1", icon="1️⃣")
+st.page_link("", label="Page 2", icon="2️⃣", disabled=True)
 st.page_link("http://www.google.com", label="Google", icon="🌎")
