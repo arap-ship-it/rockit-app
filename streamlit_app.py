eca8817aeb83e25938bdecd94f7f41d3a2c7256f
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-21 15:04:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,5 +17,5 @@ if selected is not None:
 
 st.page_link("streamlit_app.py", label="Home", icon="🏠")
 st.page_link("page1.py", label="Page 1", icon="1️⃣")
-st.page_link("", label="Page 2", icon="2️⃣", disabled=True)
+st.page_link("page2.py", label="Page 2", icon="2️⃣", disabled=True)
 st.page_link("http://www.google.com", label="Google", icon="🌎")
