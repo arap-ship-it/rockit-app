608fa2626baca5278a62952025f45e47bfbd02d8
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-21 14:26:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,9 @@ audio_value = st.audio_input("Record a voice message")
 
 if audio_value:
     st.audio(audio_value)
+import streamlit as st
+
+sentiment_mapping = ["one", "two", "three", "four", "five"]
+selected = st.feedback("stars")
+if selected is not None:
+    st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
