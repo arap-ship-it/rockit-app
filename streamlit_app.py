fddd7e57dd558db4ffa265c60f48ea9c7e218368
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-21 15:08:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ st.title("~BARUDAWGH~")
 st.write(
     "BEJA KA BARUDAWGH ONE KOMANDO"
 )
-st.page_link("streamlit_app.py", label="Home", icon="🏠")
+st.page("streamlit_app.py", label="Home", icon="🏠")
 st.page_link("page1.py", label="Page1.py", icon="1️⃣")
 st.page_link("page2.py", label="Page2.py", icon="2️⃣", disabled=True)
 st.page_link("http://www.google.com", label="Google", icon="🌎")
