173d23c0ec8042195432e341c358a0010a0c56a8
arap-ship-it <rafasyahiranp@gmail.com>
2025-05-21 15:07:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,11 @@ st.title("~BARUDAWGH~")
 st.write(
     "BEJA KA BARUDAWGH ONE KOMANDO"
 )
+st.page_link("streamlit_app.py", label="Home", icon="🏠")
+st.page_link("page1.py", label="Page1.py", icon="1️⃣")
+st.page_link("page2.py", label="Page2.py", icon="2️⃣", disabled=True)
+st.page_link("http://www.google.com", label="Google", icon="🌎")
+
 st.image("aleta02150.jpg")
 st.balloons() 
 st.badge("NO1")
@@ -13,8 +18,3 @@ sentiment_mapping = ["one", "two", "three", "four", "five"]
 selected = st.feedback("stars")
 if selected is not None:
     st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
-    import streamlit as st
-st.page_link("streamlit_app.py", label="Home", icon="🏠")
-st.page_link("page1.py", label="Page1.py", icon="1️⃣")
-st.page_link("page2.py", label="Page2.py", icon="2️⃣", disabled=True)
-st.page_link("http://www.google.com", label="Google", icon="🌎")
